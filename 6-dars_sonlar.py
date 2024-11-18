4a1c006713020d68669f865fe1789de1723b0292
ilkhom_suxrobovich <85116322+Ilkhom89@users.noreply.github.com>
2024-11-18 14:05:04 +0500

    Add files via upload

---
+++ 6-dars_sonlar.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  9 03:36:21 2021
+
+@author: ADMIN
+"""
+
+#  Integer ----- butun son
+#  Floating point number ----- suzuvchi nuqtali sonlar 
+#  Konstant konstanta -------- o'zgarmas qiymatlar pythonda katta harflar bilan yoziladi
+
+# x = 58
+# print(type(x))
+
+# aholi_soni = 7_594_458_458
+# print("Aholi soni : ", aholi_soni," ga teng")
+
+# x , y, z = 107, 8775, -56
+#
+# j = x*y
+# print(j)
+# print(type(j))
+
+# PI = 3.14159
+# radius = 20
+# diametri = 2*radius
+# print(diametri)
+# print("Aylana uzunligi ", diametri*PI , " ga teng")
+
+# ism = "Kobil"
+# yosh = 38
+# xabar = ism + " " + str(yosh) + " yoshda"
+# print(xabar)
+
+# t_yil = int(input('Tug\'ilgan yilingizni kiriting: '))
+# yosh = 2021 - t_yil
+# print("Siz" , yosh, "yoshdasiz")
+
+#################  AMALIYOT #################
+
+# son = int(input("Istalgan sonni kiriting :\n>>>"))
+# print(son , "ning kvadrati"  , son**2 , "ga teng")
+# print(son, "ning kubi" , son**3 , "ga teng")
+
+# yosh = int(input("yoshingiz nechida ? \n>>>"))
+# print("Siz", 2021 - yosh ,"- yilda tug'ilgansiz")
+
+a = float(input("Birinchi sonni kiriting : "))
+b  = float(input("Ikkinchi sonni kiriting : "))
+x = str(a)
+y = str(b)
+print(x, "+", y, "=" , a+b)
+print(x, "-", y, "=", a-b)
+print(x, "*", y, "=", a*b)
+print(x, "/", y, "=", a/b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
