5ab08884627aadb08979a62b604ca29f1c2100f1
ilkhom_suxrobovich <85116322+Ilkhom89@users.noreply.github.com>
2024-11-18 14:07:27 +0500

    Add files via upload

---
+++ 4-dars.o'zgaruvchilar.py
@@ -0,0 +1,20 @@
+
+"""
+Created on Thu May  6 07:45:59 2021vgn
+
+@author: ADMIN
+"""
+salom = 'Hello World !'
+print(salom)
+
+xabar = 'meni bugun dam olish kunim'
+print(xabar)
+xabar = '06-aprel meni dam olish kunim'
+print(xabar)
+xabar = (6**2+7**2-18+5*5)**(1/2)
+print(xabar)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print('radiusi', radius, 'bo\'lgan aylana yuzi=',aylana_yuzi)
