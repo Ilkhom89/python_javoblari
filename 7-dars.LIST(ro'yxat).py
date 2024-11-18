d5bfcdf63124c57a97bd4d115a066dabae5b9a31
ilkhom_suxrobovich <85116322+Ilkhom89@users.noreply.github.com>
2024-11-18 14:05:26 +0500

    Add files via upload

---
+++ 7-dars.LIST(ro'yxat).py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 03:20:33 2021
+
+@author: ADMIN
+"""
+
+mevalar =["olma", "anjir",'shaftoli',"o'rik"]
+print(mevalar)
+# narhlar =[12000,15000,20000,18000]
+          
+# print(mevalar[0])
+# print(mevalar[0].title())
+
+
+##############.append() ro'yxatni oxiriga element qo'shadi
+# mevalar.append('anor') 
+# print(mevalar)
+
+##############.insert() hohlagan joyga element qo'shadi
+# mevalar.insert(2,"gilos")
+# print(mevalar)
+
+############  del ro'y_ nomi[indexsi] ########### 
+          #  elementni o'chiradi #
+          
+########3 .remove()   ####   indexsi aniq bo'lmagan elementni o'chiradi
+mevalar.remove('anjir') 
+print(mevalar)
+
+############# .pop()  ############## elementni sug'irib oladi
+# bozorlik = ["yog'", "un", "piyoz","banan", 'go\'sht']
+# mahsulot = bozorlik.pop(3)
+# print("Men " + mahsulot + " sotib oldim")
+# print("Olinmagan mahsulotlar : " , bozorlik)
+
+
+
+
+
+
+############# AMALIYOT ####################
+
+# ismlar = ["Ahmad","Sanjar","Sharof","Izzat","Jahongir"]
+# print(" Salom " + ismlar.pop(0) +  " , " + "Qarshiga yaxshi yetib oldizmi ?")
+# print(" Salom " + ismlar.pop(0) + " , " + " ishlar bilan qiynalmayabsanmi ? ")
+# print(" Salom " +  ismlar.pop(0) + " , " + "bugun oshga boramizmi ? ")
+# print(" Salom " + ismlar.pop(0) + " , " + " jiyancha qanday , katta bo'layabdimi ?")
+# print(" Salom " + ismlar.pop(0) + " , " + " Koreadan qachon kelasan ?") 
+
+# sonlar = [23,-98,56,45.2,3]
+# print(sonlar[0] + sonlar[1])
+# print(sonlar[2] * sonlar[4])
+# print(sonlar[3] / 2 )
+# sonlar[0] = 100
+# print(sonlar)
+# sonlar[2] = sonlar[2] + 44
+# print(sonlar)
+
+# t_shaxslar = ["Amir Temur","Alisher Navoiy","Ibn Sino","Mirzo Ulug'bek"]
+# z_shaxslar = ["Shavkat Miromonovich","Abdullo Aripov","Po'lat Razzoqovich"]
+# print("Men ", " tarixiy shaxslardan ", t_shaxslar.pop(0), 
+#      " bilan va zamonamiz shaxsi bo'lgan ", z_shaxslar.pop(0), 
+#      " bilan yonma- yon turib gaplashishni hohlayman. ")
+
+# friends = []
+# friends.append("Ahmad")
+# friends.append("Sharof")
+# friends.append("Sanjar")
+# friends.insert(1,"Sobir")
+# friends.insert(2,"Jahongir")
+# friends.insert(0,"Ikrom")
+# print(friends)
+# friends.remove("Jahongir")
+# print(friends)
+# friends.append("Abror")
+# friends.insert(0,"Izzat")
+# friends.insert(3,"Otajon")
+# print(friends)
+
+# mehmonlar = []
+# mehmonlar.append(friends.pop(1))
+# mehmonlar.append(friends.pop(3))
+# print("\nKelgan mehmonlar :" , mehmonlar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
